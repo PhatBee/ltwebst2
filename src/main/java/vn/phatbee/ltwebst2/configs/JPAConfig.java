fa880d5f452d4578e136496c51b60c28a27b3926
PhatBee <vinhphatst1235@gmail.com>
2024-10-08 00:33:02 +0700

    JPA Pom: Hibernate Create JPAConfig, persistence

---
+++ src/main/java/vn/phatbee/ltwebst2/configs/JPAConfig.java
@@ -0,0 +1,13 @@
+package vn.phatbee.ltwebst2.configs;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+public class JPAConfig {
+    public static EntityManager getEntityManager() {
+        EntityManagerFactory factory = Persistence.createEntityManagerFactory("phatbee");
+        return factory.createEntityManager();
+
+    }
+}
